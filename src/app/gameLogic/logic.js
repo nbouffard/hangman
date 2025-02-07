be28320d5706e9c5d6b26b5a4c9bd2df37ea335f
Nicola Bouffard <nicola.bouffard855@gmail.com>
2025-02-07 14:49:15 +0100

    adding tests

--- src/app/gameLogic/logic.js
+++ src/app/gameLogic/logic.js
@@ -1,3 +1,27 @@
+let numOfMistakes = 7
+
 const testFunction = () => 1
 
-export { testFunction }
+const words = ['duncan', 'mittens', 'coco', 'kenco', 'kitty', 'benji', 'mazikeen']
+
+const getWord = (arrayOfWords = words, overridingWord) => {
+  const randomIndex = Math.floor(Math.random() * arrayOfWords.length)
+  return overridingWord || arrayOfWords[randomIndex];
+}
+
+const mistakeCounter = () => {
+  return numOfMistakes
+}
+
+let selectedWord = getWord();
+
+const hiddenWord = () => {
+  const underscoreWord = selectedWord.split('').map(() => '_').join('')
+  return underscoreWord
+}
+
+const checkLetter = (letter, myWord = selectedWord) => {
+  return myWord.split('').includes(letter)
+}
+
+export { testFunction, getWord, words, mistakeCounter, selectedWord, hiddenWord, checkLetter }
--- src/app/gameLogic/logic.test.js
+++ src/app/gameLogic/logic.test.js
@@ -1,28 +1,41 @@
 import { expect, test } from 'vitest'
 // import { render, screen } from '@testing-library/react'
-import { testFunction } from "./logic"
+import { testFunction, getWord, words, mistakeCounter, hiddenWord, selectedWord, checkLetter  } from "./logic"
 
 test('it works', () => {
   expect(testFunction()).toBeDefined()
   expect(testFunction()).toBe(1)
 })
 
-test.skip('should select random word', () => {
+test('#getWord - should select random word', () => {
+  expect(getWord()).toBeDefined()
+  expect(words).toContain(getWord())
+  // expect(getWord(words)).toEqual(expect.any(words))
 })
 
-test.skip('Should have a default value of 7', () => {
+test('#mistakeCounter - Should have a default value of 7', () => {
+  expect(mistakeCounter()).toBeDefined()
+  expect(mistakeCounter()).toBe(7)
 })
 
-test.skip("Length of '_' should match length of word", () => {
+test("#hiddenWord - Length of '_' should match length of selectedWord", () => {
+  expect(hiddenWord()).toBeDefined()
+  expect(hiddenWord().length).toEqual(selectedWord.length)
 })
 
-test.skip('Should check if the letter is in the word', () => {
+test('#checkLetter - Should check if the letter is in the word', () => {
+  const myWord = getWord(words, 'whatever')
+  expect(checkLetter('w', myWord)).toBeDefined()
+  expect(checkLetter('w', myWord)).toBe(true)
 })
 
-test.skip('Should check if the letter is NOT the word', () => {
+test('#checkLetter - Should check if the letter is NOT the word', () => {
+  const myWord = getWord(words, 'whatever')
+  expect(checkLetter('z', myWord)).toBeDefined()
+  expect(checkLetter('z', myWord)).toBe(false)
 })
 
-test.skip('Should not add more letterd to the board if game is lost', () => {
+test.skip('Should not add more letters to the board if game is lost', () => {
 })
 
 test.skip('Should not add more letters to the board if game is won', () => {
