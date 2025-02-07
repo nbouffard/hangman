38fb723c37153282c5b568f187778b5b6d35bc54
Nicola Bouffard <nicola.bouffard855@gmail.com>
2025-02-07 13:12:04 +0100

    add test descriptions

--- src/app/gameLogic/logic.test.js
+++ src/app/gameLogic/logic.test.js
@@ -7,5 +7,29 @@ test('it works', () => {
   expect(testFunction()).toBe(1)
 })
 
-test.skip('it works', () => {
+test.skip('should select random word', () => {
+})
+
+test.skip('Should have a default value of 7', () => {
+})
+
+test.skip("Length of '_' should match length of word", () => {
+})
+
+test.skip('Should check if the letter is in the word', () => {
+})
+
+test.skip('Should check if the letter is NOT the word', () => {
+})
+
+test.skip('Should not add more letterd to the board if game is lost', () => {
+})
+
+test.skip('Should not add more letters to the board if game is won', () => {
+})
+
+test.skip('Should check if the game has won', () => {
+})
+
+test.skip('New word should be selected if restart has been called', () => {
 })
