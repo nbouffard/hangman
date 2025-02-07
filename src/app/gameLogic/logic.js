f2ef2013291a453360a37d74199e8d1218f52a7f
Nicola Bouffard <nicola.bouffard855@gmail.com>
2025-02-07 13:10:55 +0100

    add test descriptions

---
+++ src/app/gameLogic/logic.js
@@ -0,0 +1,3 @@
+const testFunction = () => 1
+
+export { testFunction }
--- src/app/gameLogic/logic.test.js
+++ src/app/gameLogic/logic.test.js
@@ -1,2 +1,11 @@
 import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
+// import { render, screen } from '@testing-library/react'
+import { testFunction } from "./logic"
+
+test('it works', () => {
+  expect(testFunction()).toBeDefined()
+  expect(testFunction()).toBe(1)
+})
+
+test.skip('it works', () => {
+})
